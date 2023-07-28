9fb7243bffccb7aeaef8fbce35eedcf5eb7983b4
Rob Harding <robbie.harding@hotmail.com>
2023-07-27 20:11:52 -0400

    Collection search functionality

--- src/components/AvatarCollection.tsx
+++ src/components/AvatarCollection.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Avatar } from "@prisma/client";
-import { FC } from "react";
+import { FC, useState } from "react";
 import { Input } from "./ui/input";
 import Image from "next/image";
 import { S3_URL } from "@/constants";
@@ -11,20 +11,33 @@ interface AvatarCollectionProps {
 }
 
 const AvatarCollection: FC<AvatarCollectionProps> = ({ avatars }) => {
+  const [input, setInput] = useState<string>("");
+
   return (
     <>
-      <Input className="mt-4" placeholder="Search by prompt" />
+      <Input
+        className="mt-4"
+        value={input}
+        onChange={(e) => setInput(e.currentTarget.value)}
+        placeholder="Search by prompt"
+      />
       <div className="grid grid-cols-1 sm:grid-cols-4 md:grid-cols-6 gap-4 mt-6">
-        {avatars.map((avatar) => (
-          <div key={avatar.id} className="relative w-full pt-[100%]">
-            <Image
-              src={S3_URL + avatar.id}
-              alt={`Avatar ${avatar.id}`}
-              fill
-              className="object-cover w-full h-full inset-0 rounded-lg"
-            />
-          </div>
-        ))}
+        {avatars
+          .filter(
+            (avatar) =>
+              input.length === 0 ||
+              avatar.prompt?.toLowerCase().includes(input.toLowerCase())
+          )
+          .map((avatar) => (
+            <div key={avatar.id} className="relative w-full pt-[100%]">
+              <Image
+                src={S3_URL + avatar.id}
+                alt={`Avatar ${avatar.id}`}
+                fill
+                className="object-cover w-full h-full inset-0 rounded-lg"
+              />
+            </div>
+          ))}
       </div>
     </>
   );
