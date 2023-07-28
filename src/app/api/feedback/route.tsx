4c326fe5e07f56c96304eb676aef3581c0e58360
Rob Harding <robbie.harding@hotmail.com>
2023-07-28 08:42:51 -0400

    Feedback route

---
+++ src/app/api/feedback/route.tsx
@@ -0,0 +1,31 @@
+import { getAuthSession } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { FeedbackFormRequestSchema } from "@/lib/validators/feedback";
+import { z } from "zod";
+
+export default async function POST(req: Request) {
+  const session = await getAuthSession();
+
+  if (!session?.user) {
+    return new Response("Unauthorized", { status: 400 });
+  }
+
+  try {
+    const body = await req.json();
+    const { text } = FeedbackFormRequestSchema.parse(body);
+
+    await db.feedback.create({
+      data: {
+        text,
+        userId: session?.user.id,
+      },
+    });
+
+    return new Response("OK");
+  } catch (err) {
+    if (err instanceof z.ZodError) {
+      return new Response("Invalid request data passed", { status: 422 });
+    }
+    return new Response("Something went wrong.", { status: 500 });
+  }
+}
